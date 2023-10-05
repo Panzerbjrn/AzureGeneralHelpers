c84f07d73faaf1e8e51b8e5c760b02f51064b3e3
Lars Petersson <lars.petersson@lexacom.co.uk>
2023-10-05 15:56:03 +0100

    Refreshing

---
+++ AzureGeneralHelpers.psm1
@@ -0,0 +1,43 @@
+#region Script Header
+#	NAME: AzureGeneralHelpers.psm1
+#	AUTHOR: Lars Panzerbjørn
+#	CONTACT: lpetersson@hotmail.com / GitHub: Panzerbjrn
+#	DATE: 2023.10.05
+#	VERSION: 0.1 - 2023.10.05 - Module Created with Create-NewModuleStructure by Lars Panzerbjørn
+#
+#	SYNOPSIS:
+#
+#
+#	DESCRIPTION:
+#	Module to help with simple Azure tasks
+#
+#	REQUIREMENTS:
+#
+#endregion Script Header
+
+#Requires -Version 5.0
+
+[CmdletBinding(PositionalBinding=False)]
+param()
+
+Write-Verbose $PSScriptRoot
+
+#Get Functions and Helpers function definition files.
+$Functions	= @( Get-ChildItem -Path $PSScriptRoot\Functions\*.ps1 -ErrorAction SilentlyContinue )
+$Helpers = @( Get-ChildItem -Path $PSScriptRoot\Helpers\*.ps1 -ErrorAction SilentlyContinue )
+
+#Dot source the files
+ForEach ($Import in @($Functions + $Helpers))
+{
+	Try
+	{
+		. $Import.Fullname
+	}
+	Catch
+	{
+		Write-Error -Message "Failed to Import function $($Import.Fullname): $_"
+	}
+}
+
+Export-ModuleMember -Function $Functions.Basename
+
