5d3bc8482a040243ce09b527ebb8ae9153186d29
Lars Petersson <lars.petersson@lexacom.co.uk>
2023-10-12 09:54:14 +0100

    Refreshing

--- AzureGeneralHelpers/Functions/List-AzComputeResourceSku.ps1
+++ AzureGeneralHelpers/Functions/List-AzComputeResourceSku.ps1
@@ -23,74 +23,47 @@ Function List-AzComputeResourceSku {
 #>
 	[CmdletBinding()]
 	param(
-		[Parameter(Mandatory=$false,
-			ValueFromPipeline=$True,
+		[Parameter(ValueFromPipeline=$True,
 			ValueFromPipelineByPropertyName=$True,
 			HelpMessage='Which region do you want information about?')]
 		[Alias('Location','AzRegion','Region')]
 		[string]$AzLocation,
 
-		[Parameter(Mandatory=$false,
-			ValueFromPipeline=$True,
+		[Parameter(ValueFromPipeline=$True,
 			ValueFromPipelineByPropertyName=$True,
 			HelpMessage='Which Type of resource Sku do you want information about?')]
 		[Alias('Type')]
-		[string]$AzResourceType
+        [ValidateSet("availabilitySets","disks","hostGroups/hosts","snapshots","virtualMachines")]
+		[string]$AzResourceType,
+
+        [switch]$Ask
 	)
 
-	BEGIN{
-		Write-Verbose "Beginning $($MyInvocation.Mycommand)"
-	}
+    Write-Verbose "Processing $($MyInvocation.Mycommand)"
 
-	PROCESS{
-		Write-Verbose "Processing $($MyInvocation.Mycommand)"
+    IF((!$AzLocation) -and ($Ask)){
+        $menu = @{}
+        $Items =  Get-AzLocation | select DisplayName | Sort -Property DisplayName
+        for ($i=1;$i -le $Items.count; $i++) {
+            Write-Host "$i. $($Items[$i-1].DisplayName)"
+            $menu.Add($i,($Items[$i-1].DisplayName))
+            }
 
-		IF((!$AzLocation) -and ($Ask)){
-			$menu = @{}
-        	$Items =  Get-AzLocation | select DisplayName | Sort -Property DisplayName
-			for ($i=1;$i -le $Items.count; $i++) {
-				Write-Host "$i. $($Items[$i-1].DisplayName)"
-				$menu.Add($i,($Items[$i-1].DisplayName))
-				}
-
-			[int]$ans = Read-Host 'Enter selection'
-			$AzLocation = $menu.Item($ans)
-		}
-
-        IF($AzStorageAccount){$AzStorageAccount = Get-AzStorageAccount -ResourceGroupName $AzResourceGroup -Name $AzStorageAccount}
-		IF(!$AzStorageAccount){
-			$menu = @{}
-        	$Items = Get-AzStorageAccount -ResourceGroupName $AzResourceGroup | Sort-Object -Property StorageAccountName
-			for ($i=1;$i -le $Items.count; $i++) {
-				Write-Host "$i. $($Items[$i-1].StorageAccountName)"
-				$menu.Add($i,($Items[$i-1].StorageAccountName))
-			}
-
-			[int]$ans = Read-Host 'Enter selection'
-			$AzStorageAccount = $menu.Item($ans)
-		}
-
-        IF($AzStoragecontainer){
-            $AzStoragecontainer = Get-AzStorageContainer -Context (Get-AzStorageAccount -ResourceGroupName $AzResourceGroup -Name $AzStorageAccount).Context -Name $AzStoragecontainer | Select-Object -ExpandProperty Name
-        }
-        IF(!$AzStoragecontainer){
-			$menu = @{}
-        	$Items = Get-AzStorageContainer -Context (Get-AzStorageAccount -ResourceGroupName $AzResourceGroup -Name $AzStorageAccount).Context | Sort-Object -Property Name
-			for ($i=1;$i -le $Items.count; $i++) {
-				Write-Host "$i. $($Items[$i-1].Name)"
-				$menu.Add($i,($Items[$i-1].Name))
-			}
-
-			[int]$ans = Read-Host 'Enter selection'
-			$AzStoragecontainer = $menu.Item($ans)
-		}
-
-
-        Get-AzStorageBlob -Context (Get-AzStorageAccount -ResourceGroupName $AzResourceGroup -Name $AzStorageAccount).Context -Container $AzStoragecontainer | Select-Object -ExpandProperty Name
-
+        [int]$ans = Read-Host 'Enter selection'
+        $AzLocation = $menu.Item($ans)
     }
 
-	END{
-		Write-Verbose "Ending $($MyInvocation.Mycommand)"
-	}
+    IF($AzLocation){
+        $Skus = Get-AzComputeResourceSku -Location $AzLocation
+    }
+    ELSE{
+        $Skus = Get-AzComputeResourceSku
+    }
+
+    IF($AzResourceType){
+        $Skus | Where-Object { $_.ResourceType -match $AzResourceType }
+    }
+    ELSE{
+        $Skus
+    }
 }
